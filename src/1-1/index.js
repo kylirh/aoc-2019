3cc3f29850dc14da3b407408da48c6cfc225e8b0
Kylir Horton <kylirh@gmail.com>
2019-12-02 17:24:25 -0700

    Day 1, challenge 1

--- src/1-1/index.js
+++ src/1-1/index.js
@@ -1,5 +1,10 @@
-function hello(data) {
-  return `Hello ${data}`;
-}
+export const getFuelNeeded = (mass) => {
+  return Math.floor(mass / 3) - 2;
+};
 
-export default hello;
+export default (data) => {
+  return data
+    .split(/[\r\n]+/)
+    .map((x) => getFuelNeeded(parseInt(x, 10)))
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+};
--- src/1-1/index.test.js
+++ src/1-1/index.test.js
@@ -1,9 +1,18 @@
 import { expect } from 'chai';
-import hello from './index.js';
+import { getFuelNeeded } from './index.js';
 
-describe('hello function', () => {
-  it('creates a greeting', () => {
-    const greeting = hello('Kylir Horton');
-    expect(greeting).to.equal('Hello Kylir Horton');
+describe('day 1, challenge 1', () => {
+  it('calculates needed fuel correctly', () => {
+    let fuelNeeded = getFuelNeeded(12);
+    expect(fuelNeeded).to.equal(2);
+
+    fuelNeeded = getFuelNeeded(14);
+    expect(fuelNeeded).to.equal(2);
+
+    fuelNeeded = getFuelNeeded(1969);
+    expect(fuelNeeded).to.equal(654);
+
+    fuelNeeded = getFuelNeeded(100756);
+    expect(fuelNeeded).to.equal(33583);
   });
 });
